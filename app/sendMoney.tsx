11d255a6a7b3af34d88f1c22dff4dd004907759c
Solomon Udofia <meetsolomonudofia@gmail.com>
2024-06-05 20:29:11 +0100

    installed the SelectList dependency to fetch bank names from an API and displayed in th SelectList drop down

--- app/sendMoney.tsx
+++ app/sendMoney.tsx
@@ -1,43 +1,82 @@
 import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
-import KeyPad from '../components/keyPad';
 import { FontAwesome6 } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { SelectList } from 'react-native-dropdown-select-list';
+
+type Bank = {
+    name: string;
+    code: string;
+}
 
 const SendMoney = () => {
 
-    const handlePress = () => {
-    }
+    const navigate = useNavigation()
+    const [banks, setBanks] = useState<Bank[]>([])
+    const [selectedAccount, setSelectedAccount] = useState<string>('')
+    const [selectedBank, setSelectedBank] = useState<string>('')
 
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <LinearGradient
-        colors={['#290067', '#4600AC']}
-        start={{x:0, y: 0}}
-        end={{x:1, y: 0}}
-      >
-        <ScrollView 
-          showsVerticalScrollIndicator={false} 
-        >
-            <View className=''>
-                <View className='flex-row px-5 py-10'>
-                    <TouchableOpacity>
-                        <FontAwesome6 name='angle-left' color='#fff' size={28}/>
-                    </TouchableOpacity>
-                    <Text className='text-white text-2xl font-bold text-center w-full'>Send Money</Text>
+    const accountName = [
+        {id: 1, name: 'Saving'},
+        {id: 2, name: 'Current'},
+        {id: 3, name: 'Domiciliary'}
+    ]
+
+    useEffect(() => {
+        getBanks();
+    }, []);
+
+    const getBanks = async () => {
+        try {
+            const response = await fetch('https://nigerianbanks.xyz');
+            const result = await response.json()
+            setBanks(result)
+        } catch (error) {
+            console.error('Error fetching banks:', error)
+        }
+    };
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView
+                showsVerticalScrollIndicator={false} >
+                <LinearGradient
+                    colors={['#290067', '#4600AC']}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 1, y: 0 }}
+                >
+                    <View className='flex-row px-5 py-7'>
+                        <TouchableOpacity onPress={() => { navigate.goBack() }}>
+                            <FontAwesome6 name='angle-left' color='#fff' size={28} />
+                        </TouchableOpacity>
+                        <Text className='text-white text-2xl font-bold text-center w-full'>Send Money</Text>
+                    </View>
+                    <View className=' p-3 bg-white rounded-t-3xl '>
+                        <Text className='p-1'>From Account</Text>
+                        <SelectList
+                            setSelected={(val: string) => setSelectedAccount(val)}
+                            data={accountName.map((account) => ({ key: account.id, value: account.name }))}
+                            placeholder='Select account to debit'
+                            search={false}
+                        />
+                    </View>
+                </LinearGradient>
+                <View className='h-full bg-white'>
+                    <View className=' p-3 bg-white rounded-t-3xl '>
+                        <Text className='p-1'>To Bank</Text>
+                        <SelectList
+                            setSelected={(val: string) => setSelectedBank(val)}
+                            data={banks.map((bank: Bank) => ({ key: bank.code, value: bank.name }))}
+                            placeholder='Select Bank'
+                            notFoundText='Bank is not found'
+                        />
+                    </View>
                 </View>
-               <View className='bg-white rounded-t-3xl flex-1 h-full'>
-               <View className=' px-5 py-10'>
-                    <Text className='text-3xl border border-gray-300 rounded-2xl p-3'>O</Text>
-                </View>
-                <KeyPad onPress={handlePress} />
-               </View>
-            </View>
-        </ScrollView>
-      </LinearGradient>
-    </SafeAreaView>
-  );
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 export default SendMoney;
--- components/keyPad.tsx
+++ components/keyPad.tsx
@@ -2,58 +2,57 @@ import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 import { FontAwesome, FontAwesome6 } from '@expo/vector-icons'
 
-const KeyPad = () => {
+interface pressType {
+    onPress: ()=>void
+}
 
-    interface pressType {
-        onPress: ()=>void
-    }
 
-    const handlePress = ({onPress}: pressType) => {}
+const KeyPad = ({onPress}: pressType) => {
 
   return (
     <ScrollView className='px-5 bg-white h-full'>
        <View className='flex-row justify-between max-w-[680px] mb-10'>
-       <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>9</Text>
+       <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>9</Text>
     </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>8</Text>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>8</Text>
     </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>7</Text>
-    </TouchableOpacity>
-       </View>
-       <View className='flex-row justify-between max-w-[680px] mb-10'>
-       <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>6</Text>
-    </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>10</Text>
-    </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>4</Text>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>7</Text>
     </TouchableOpacity>
        </View>
        <View className='flex-row justify-between max-w-[680px] mb-10'>
-       <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>10</Text>
+       <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>6</Text>
     </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>2</Text>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>5</Text>
     </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>1</Text>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>4</Text>
     </TouchableOpacity>
        </View>
        <View className='flex-row justify-between max-w-[680px] mb-10'>
-       <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>.</Text>
+       <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>3</Text>
     </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text className='text-2xl'>0</Text>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>2</Text>
     </TouchableOpacity>
-    <TouchableOpacity className='bg-slate-200 text-black w-12 h-12 items-center justify-center rounded-full'>
-      <Text><FontAwesome6 name='delete-left' size={22}/> </Text>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>1</Text>
+    </TouchableOpacity>
+       </View>
+       <View className='flex-row justify-between max-w-[680px] mb-10'>
+       <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>.</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <Text className='text-2xl text-gray-500'>0</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={onPress} className='w-12 h-12 items-center justify-center '>
+      <FontAwesome6 name='delete-left' size={22}/> 
     </TouchableOpacity>
        </View>
     </ScrollView>
